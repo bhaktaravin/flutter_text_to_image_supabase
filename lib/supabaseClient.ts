d6dc4ffd5f0b2b6cb4ff3d550149005db7ad63ae
Ravin Bhakta <ravin.bhakta@gmail.com>
2025-09-16 22:19:12 -0700

    removed secrets from file

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://utcvdlqypjgrgnesbzyg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Y3ZkbHF5cGpncmduZXNienlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MzkzMDUsImV4cCI6MjA3MjQxNTMwNX0.vAUMNpa0UAHtKucsWHEAmoL1pRByMVJXHXk0BnV5QhI';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
